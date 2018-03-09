3143dec348c3a41cb8b075900830824811d836dd
Javier Velasquez Lopez <javier@JavierVelasquez.local>
2018-03-09 11:03:06 +0000

    Test Kitchen for Terraform

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "docker" {
+  host = "unix://localhost/var/run/docker.sock"
+}
+
+data "docker_registry_image" "ubuntu" {
+  name = "rastasheep/ubuntu-sshd:latest"
+}
+
+resource "docker_image" "ubuntu" {
+  name          = "${data.docker_registry_image.ubuntu.name}"
+  pull_triggers = ["${data.docker_registry_image.ubuntu.sha256_digest}"]
+}
+
+resource "docker_container" "ubuntu" {
+  image    = "${docker_image.ubuntu.name}"
+  must_run = true
+  name     = "ubuntu_container"
+
+  ports {
+    external = 2222
+    internal = 22
+  }
+}
